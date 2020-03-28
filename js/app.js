d7f34dfc20cafe2db3b93c5550722ab80e3dc92a
africalib <africalibrary21@gmail.com>
2020-03-29 05:22:17 +0900

    first

---
+++ js/app.js
@@ -0,0 +1,34 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+    methods: {
+        watch: function () {
+            var hashArr = location.hash.split('/');
+            var val1 = '';
+            var val2 = '';
+            var val3 = '';
+
+            if (hashArr.length > 1)
+                val1 = hashArr[1];
+
+            if (hashArr.length > 2)
+                val2 = hashArr[2];
+
+            if (hashArr.length > 3)
+                val3 = hashArr[3];
+
+            console.log(val1);
+            console.log(val2);
+            console.log(val3);
+        }
+    },
+    created: function () {
+        var t = this;
+        t.watch();
+        window.onhashchange = function () {
+            t.watch();
+        }
+    }
+});
